3de36525d0b50c0b9f293f16e48a369dd6749842
LazzyIzzi <78691958+LazzyIzzi@users.noreply.github.com>
2023-04-07 16:45:46 -0400

    Edit multiple links and content

--- GenericDialogAddin/package-search-index.js
+++ GenericDialogAddin/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jhd.AddIns"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"jhd.ImageJAddins"}];updateSearchResults();
